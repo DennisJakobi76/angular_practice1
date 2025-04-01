a66e4e5dd71de3bcc02f5cd2fc4fb660fe36ad90
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-04-01 14:44:30 +0200

    added type-alias and interface to user-component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,6 +8,18 @@ import {
   output,
 } from '@angular/core';
 
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// };
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -16,16 +28,17 @@ import {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return `assets/users/${this.avatar}`;
+    return `assets/users/${this.user.avatar}`;
   }
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 
   // select = output<string>();
