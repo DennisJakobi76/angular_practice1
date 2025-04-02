ee983fcd73d72f4c601d7678e37f044834d6a40f
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-04-02 16:15:38 +0200

    added shared card-component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [CardComponent],
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
